93c4d4cb2e65b878f85b7263bbc6f2185e69d116
Roberto Nares <roberto.nares@gmail.com>
2024-07-23 11:55:38 -0700

    Use the demo form letter erb template to create individualized thank you letters using civicinfo google api response data

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,9 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5, '0')[0..4]
+  zipcode.to_s.rjust(5,"0")[0..4]
 end
 
 def legislators_by_zipcode(zip)
@@ -10,17 +11,23 @@ def legislators_by_zipcode(zip)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody'],
-      options: { retries: 5 }
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(', ')
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
   end
 end
 
@@ -32,12 +39,16 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
-  
-  puts "#{name} #{zipcode} #{legislators}"
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id,form_letter)
 end
